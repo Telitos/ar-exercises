a76e9c711f582d34f362682ee1e12dec54d5dc2e
Ghislain H <telitos@gmail.com>
2016-12-07 00:58:57 +0000

    completes exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,18 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@allstores = Store.all
+@allstores_revenue = Store.all.sum(:annual_revenue)
+
+puts "Total revenue #{@allstores_revenue}"
+
+@average_revenue = @allstores_revenue/@allstores.size
+
+puts "Number of stores: #{@allstores.size}"
+
+puts "average revenue: #{@average_revenue}"
+Inf = 1.0/0.0
+@Million_dollar_stores = Store.where(annual_revenue: (1000000..Inf))
+
+puts "There are #{@Million_dollar_stores.size} stores that make over 1M in annual revenue"
+
